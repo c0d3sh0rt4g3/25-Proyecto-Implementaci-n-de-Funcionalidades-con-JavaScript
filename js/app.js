7c16bae6f123b7ad8d58adf73b00088dc1cd3b83
out0f4mm0 <eriking2004@gmail.com>
2023-11-03 12:46:34 +0100

    Half done main page functionalities

--- js/app.js
+++ js/app.js
@@ -35,14 +35,43 @@ const insertClientsIntoPage = (clients) => {
         phoneCell.textContent = client.telefono
         enterpriseCell.textContent = client.empresa
 
-        const deleteButton = document.createElement('button')
-
-        deleteButton.textContent = 'borrar cliente'
+        const deleteButton = document.createElement("button")
+        deleteButton.textContent = "borrar cliente"
         deleteButton.classList.add("delete-button", "bg-red-500", "hover:bg-red-600", "font-bold","px-2", "rounded-full")
-        deleteButton.addEventListener('click', () => {
-            newRow.remove()
+
+        const editClientLink = document.createElement("a")
+        editClientLink.href = "./editar-cliente.html"
+
+        const editButton = document.createElement("button")
+        editButton.textContent = "editar cliente"
+        editButton.classList.add("delete-button", "bg-green-500", "hover:bg-green-500", "font-bold","px-2", "rounded-full")
+        editClientLink.appendChild(editButton)
+        editButton.addEventListener("click", () =>{
+            localStorage.setItem("clientToEditId", client.id)
         })
+
+        deleteButton.addEventListener("click", () => {
+            newRow.remove()
+            const dbName = "ClientDB"
+            const request = window.indexedDB.open(dbName, 1)
+
+            request.onsuccess = (e) => {
+                const db = e.target.result
+                const transaction = db.transaction("clients", "readwrite")
+                const objectStore = transaction.objectStore("clients")
+                const deleteRequest = objectStore.delete(client.id)
+                deleteRequest.onsuccess = () => {
+                    console.log("Cliente eliminado de IndexedDB")
+                }
+
+                deleteRequest.onerror = (error) => {
+                    console.error("Error al borrar el cliente de IndexedDB:", error)
+                }
+            }
+        })
+
         actionsCell.appendChild(deleteButton)
+        actionsCell.appendChild(editClientLink)
     })
 }
 document.addEventListener("DOMContentLoaded", showAllClientsFromDB)
